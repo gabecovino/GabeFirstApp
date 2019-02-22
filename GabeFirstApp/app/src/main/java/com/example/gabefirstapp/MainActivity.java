7535bd5412d1eba35145f6c9bb38304e03049147
gabecovino <vcovi6245@westada.org>
2019-02-22 12:16:54 -0700

    fixed bug with doubles, fully functional

--- GabeFirstApp/app/src/main/java/com/example/gabefirstapp/MainActivity.java
+++ GabeFirstApp/app/src/main/java/com/example/gabefirstapp/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.gabefirstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addBtn = (Button) findViewById(R.id.addBtn);
+        Button multBtn = (Button) findViewById(R.id.multBtn);
+        Button subBtn = (Button) findViewById(R.id.subBtn);
+        Button divBtn = (Button) findViewById(R.id.divBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                double result = num1 + num2;
+                resultTextView.setText(result + "");
+            }
+
+        });
+        subBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                double result = num1 - num2;
+                resultTextView.setText(result + "");
+            }
+
+        });
+        multBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                double result = num1 * num2;
+                resultTextView.setText(result + "");
+            }
+
+        });
+        divBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
+                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                double num1 = Double.parseDouble(firstNumEditText.getText().toString());
+                double num2 = Double.parseDouble(secondNumEditText.getText().toString());
+                double result = num1 / num2;
+                resultTextView.setText(result + "");
+            }
+
+        });
     }
 }
